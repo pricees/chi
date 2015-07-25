ef404a14b0e032dde692b017a36e80aef8378736
Ted Price <ted.price@gmail.com>
2015-07-25 00:25:10 -1000

    No tests, but I am feeling confident!

--- instant/instant.go
+++ instant/instant.go
@@ -3,8 +3,53 @@ package instant
 import (
   "fmt"
   "net/http"
+_  "errors"
+  "regexp"
 )
 
+type RouteHandler struct {
+  routeMatcher *regexp.Regexp
+  handlerFunc func(http.ResponseWriter, *http.Request)
+}
+
+type RouteHandlers []*RouteHandler
+
+func (r RouteHandlers) findRouteHandler(url string) (*RouteHandler, bool){
+  for _, routeHandler := range r {
+    if routeHandler.routeMatcher.Match([]byte(url)) {
+      return routeHandler, true
+    }
+  }
+  return nil, false
+
+}
+
+func (r RouteHandlers) Route(url string, handler func(http.ResponseWriter, *http.Request)) RouteHandlers {
+  return append(r, &RouteHandler{ regexp.MustCompile(fmt.Sprintf("^%s%$", url)), handler} )
+}
+
+type RouteTable map[string]RouteHandlers
+
+func (r *RouteTable) Route(method string, url string, handler func(http.ResponseWriter, *http.Request)) {
+    if _, exists := (*r)[method]; !exists {
+      (*r)[method] = make(RouteHandlers, 0, 100)
+    }
+
+    if _, exists := (*r)[method].findRouteHandler(url); !exists {
+      (*r)[method] = (*r)[method].Route(url, handler)
+    }
+    fmt.Println(Routes)
+}
+
+var Routes *RouteTable
+
+func init() {
+  Routes = &RouteTable{}
+}
+
+func Send(w http.ResponseWriter, text string) {
+  fmt.Fprintf(w, text)
+}
 
 func Listen(port int) {
   http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
--- server.go
+++ server.go
@@ -7,13 +7,14 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello World")
+  instant.Send(w, "Hello Jeah!")
 }
 
 func main() {
 
   p := 8080
   fmt.Printf("'instant' listening on %d\n", p)
+  instant.Routes.Route("GET", "/", helloWorld)
   instant.Get("/", helloWorld)
   instant.Listen(p)
 }
